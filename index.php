5b4ecfdee50ac75c3ac86dc462eef8f586c31fc2
KairiMorishita <kairi.morishita@person-link.co.jp>
2020-08-07 17:45:55 +0900

    削除機能追加

--- index.php
+++ index.php
@@ -1,50 +1,73 @@
 <?php
 session_start();
 
+$db['host'] = "localhost";  // DBサーバのURL
+$db['user'] = "root";  // ユーザー名
+$db['pass'] = "root";  // ユーザー名のパスワード
+$db['dbname'] = "php_keijiban";  // データベース名
+
 //ログイン状態チェック
 if(!isset($_SESSION["NAME"])){
     header("Location: Logout.php");
     exit;
 }
 
-// ファイルの指定
-$dataFile = 'datafile.dat';
-
 //エスケープする関数
 function h($s){
 return htmlspecialchars($s,ENT_QUOTES,'UTF-8');
 }
 
-//name="send_message"のPOST送信があった時
+$dsn = sprintf('mysql: host=%s; dbname=%s; charset=utf8', $db['host'], $db['dbname']);
+
+//投稿ボタンが押された場合
 if(isset($_POST["send_message"])){
     //送信されたname="message"とname="user_name"の値を取得する
     $message = trim($_POST['message']);
-    $user = trim($_POST['user_name']);
+    $user = trim($_SESSION["NAME"]);
 
-    //messageが空じゃなかったら
-    if(!empty($message)){
-
-        //userが空の場合、名無しにする
-        if(empty($user)){
-            $user = "名無し";
-        }
-        //日付を取得する
-        $postDate = date('Y-m-d H:i:s');
-        //ファイルに書き込むメッセージを作成する
-        $newData  = $message." / ".$user." / ".$postDate."\n";
-        //ファイルを開く
-        $fp = fopen($dataFile,'a');
-        //ファイルに書き込む
-        fwrite($fp,$newData);
-        //ファイルを閉じる
-        fclose($fp);
+    // 投稿内容をデータベースに保存
+    try {
+        $pdo = new PDO($dsn, $db['user'], $db['pass'], array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
+        $stmt = $pdo->prepare("INSERT INTO posts(text, user_id) VALUES (?, ?)");
+        $stmt->execute(array($message, $_SESSION["ID"]));
+    } catch(PDOException $e) {
+        $errorMessage = 'データベースエラー';
+        // $e->getMessage() でエラー内容を参照可能（デバッグ時のみ表示）
+        echo $e->getMessage();
     }
 }
-//一行ずつデータを取り出して配列に入れる
-$post_list = file($dataFile,FILE_IGNORE_NEW_LINES);
-//逆順に並べ替える
+
+// 投稿内容とユーザー名を取得
+try {
+    $pdo = new PDO($dsn, $db['user'], $db['pass'], array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
+    $sql = "SELECT text,name,user_id,posts.id FROM posts INNER JOIN users ON posts.user_id = users.id";
+    $post_list = $pdo->query($sql)->fetchALL();
+} catch(PDOException $e) {
+    $errorMessage = 'データベースエラー';
+    // $e->getMessage() でエラー内容を参照可能（デバッグ時のみ表示）
+    echo $e->getMessage();
+}
+
+// 逆順に並べ替える
 $post_list = array_reverse($post_list);
+
+//　削除ボタンが押された時
+if(isset($_POST["id"])){
+    // 投稿内容を削除
+    try {
+        $pdo = new PDO($dsn, $db['user'], $db['pass'], array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
+        $stmt = $pdo->prepare("DELETE FROM posts Where id = :id");
+        $stmt->execute(array(':id' => $_POST["id"]));
+        echo '削除完了しました。';
+    } catch(PDOException $e) {
+        $errorMessage = 'データベースエラー';
+        // $e->getMessage() でエラー内容を参照可能（デバッグ時のみ表示）
+        echo $e->getMessage();
+    }
+}
+
 ?>
+
 <!DOCTYPE html>
 <html>
 <head lang="ja">
@@ -55,7 +78,6 @@ $post_list = array_reverse($post_list);
 <body>
 <h1>PHP掲示板</h1>
 
-<!-- ユーザーIDにHTMLタグが含まれても良いようにエスケープする -->
 <p>ようこそ<u><?php echo htmlspecialchars($_SESSION["NAME"], ENT_QUOTES); ?></u>さん</p>  <!-- ユーザー名をechoで表示 -->
     <ul>
         <li><a href="Logout.php">ログアウト</a></li>
@@ -64,8 +86,7 @@ $post_list = array_reverse($post_list);
 <!--ここで投稿内容を送信する-->
 <form action="" method="post">
     投稿内容:<input type="text" name="message"><br>
-    ユーザー名:<input type="text" name="user_name"><br>
-    <input type="submit" name="send_message" value="投稿">
+            <input type="submit" name="send_message" value="投稿">
 </form>
 
 <h2>投稿一覧</h2>
@@ -74,7 +95,16 @@ $post_list = array_reverse($post_list);
 <?php if (!empty($post_list)){ ?>
     <!--post_listの中身をひとつづつ取り出し表示する-->
     <?php foreach ($post_list as $post){ ?>
-    <li><?php echo h($post); ?></li>
+    <li>
+        <?php echo $post["name"];?>
+        <?php echo $post["text"];?>
+    </li>
+    <?php if ($_SESSION["ID"] == $post["user_id"]) :?>
+        <form action="" method="post">
+            <input type="hidden" name="id" value=<?php echo $post["id"] ?>>
+            <input type="submit"  value="削除">
+        </form>
+    <?php endif; ?>
     <?php } ?>
 <?php }else { ?>
     <li>まだ投稿はありません。</li>
